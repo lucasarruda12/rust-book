d9cddb91f62c32e1ebd67095ec770ccf78ba5b1b
LucasArruda12 <lucas123manoel4@gmail.com>
2024-09-02 18:22:29 -0300

    Chapter 3 - Fibonacci

--- c3_fibonacci/src/main.rs
+++ c3_fibonacci/src/main.rs
@@ -1,3 +1,23 @@
+fn fibonacci(ind: u8) -> u32 {
+    match ind {
+        0 => return 0,
+        1 => return 1,
+        _ => (),
+    }
+
+    let mut curr: u32 = 1;
+    let mut prev: u32 = 0;
+
+    for _ in 0..ind - 1 {
+        let buff: u32 = curr;
+        curr += prev;
+        prev = buff;
+    }
+
+    curr
+}
+
 fn main() {
-    println!("Hello, world!");
+    let fib = fibonacci(20);
+    println!("{}", fib);
 }
